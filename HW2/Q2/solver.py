afc3e620b4e6737db7de8f15f0f70f54f5deff37
Jackson Baker <jackson.baker7562@gmail.com>
2025-09-24 11:22:23 -0500

    Q2 Done

--- HW2/Q2/solver.py
+++ HW2/Q2/solver.py
@@ -11,14 +11,55 @@ def print_board(board):
                 print(str(board[i][j]) + " ", end="")
     print()
 
-def find_empty(board):
-    return None
+def find_empty(board) -> (int, int):
+    for i in range(len(board)):
+        try:
+            return i, board[i].index(0), 
+        except:
+            continue
+    return -1, -1
 
-def is_valid(board, guess, pos):
-    pass
 
-def solve_sudoku(board):
-    pass
+def is_valid(board, guess, pos) -> bool:
+    row, col = pos
+
+    # Check row
+    if guess in board[row]:
+        return False
+
+    # Check column
+    for r in range(9):
+        if board[r][col] == guess:
+            return False
+
+    # Check grid
+    grid_row, grid_col = (row // 3) * 3, (col // 3) * 3
+    
+    for r in range(grid_row, grid_row + 3):
+        for c in range(grid_col, grid_col + 3):
+            if board[r][c] == guess:
+                return False
+
+    return True
+
+def solve_sudoku(board) -> bool:
+    row, col = find_empty(board)
+
+    if row == -1 and col == -1:
+        return True
+
+    for guess in range(1, 10):
+        if is_valid(board, guess, (row, col)):
+            board[row][col] = guess
+
+            if solve_sudoku(board):
+                return True
+
+            # backtrack
+            board[row][col] = 0
+
+    return False
+    
 
 sudoku_board = [
     [0,1,3,0,0,0,7,0,0],
@@ -32,11 +73,23 @@ sudoku_board = [
     [0,0,0,0,0,0,0,1,0]
 ]
 
-print("Initial Sudoku Board:")
-print_board(sudoku_board)
+another_sudoku_board = [
+    [2,6,0,0,0,8,0,0,0],
+    [0,8,0,0,0,9,6,0,0],
+    [0,0,0,0,5,0,0,0,0],
+    [0,9,4,3,0,0,5,0,0],
+    [0,0,2,0,7,0,0,0,0],
+    [0,5,0,0,0,0,8,0,4],
+    [0,3,5,8,0,0,0,0,0],
+    [0,0,0,0,0,0,3,0,1],
+    [7,0,0,0,6,0,0,0,0]
+]
 
-if solve_sudoku(sudoku_board):
+print("Initial Sudoku Board:")
+print_board(another_sudoku_board)
+
+if solve_sudoku(another_sudoku_board):
     print("Solved Sudoku Board:")
-    print_board(sudoku_board)
+    print_board(another_sudoku_board)
 else:
     print("No solution exists.")
